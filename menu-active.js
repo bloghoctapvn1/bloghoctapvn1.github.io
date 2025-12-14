362e0a9d664dc12483d80cfb46c072e96c9f4b64
bloghoctapvn <bbangtan238@gmail.com>
2025-12-14 20:00:59 +0700

    Update menu-active.js

--- menu-active.js
+++ menu-active.js
@@ -1,7 +1,7 @@
 function setActiveMenuLink() {
   const links = document.querySelectorAll("#nav-main a");
 
-  // Lấy tên file hiện tại (index.html, cach-on-thi.html...)
+  // Lấy tên file hiện tại (index.html, meo-hoc-nhanh.html, cach-on-thi.html…)
   let currentPage = window.location.pathname.split("/").pop();
   if (!currentPage) currentPage = "index.html";
 
@@ -9,14 +9,18 @@ function setActiveMenuLink() {
     const href = link.getAttribute("href");
     if (!href) return;
 
-    // Chỉ lấy tên file của link
+    // Lấy tên file của link
     const linkPage = href.split("/").pop();
 
+    // Reset trước (tránh bị active 2 cái)
+    link.classList.remove("active");
+
+    // Match chính xác
     if (linkPage === currentPage) {
       link.classList.add("active");
     }
   });
 }
 
-// Cho HTML gọi sau khi fetch menu
+// Cho HTML gọi sau khi fetch menu.html
 window.setActiveMenuLink = setActiveMenuLink;
