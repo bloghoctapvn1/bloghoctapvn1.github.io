e7c989c3fcda4e311050607ba33e620a9bb25740
bloghoctapvn <bbangtan238@gmail.com>
2025-12-15 10:52:09 +0700

    Update menu-active.js

--- menu-active.js
+++ menu-active.js
@@ -7,10 +7,6 @@
       // CÁCH 1: gọi trực tiếp (ổn nhất)
       if (window.setActiveMenuLink) window.setActiveMenuLink();
 
-      // (hoặc) CÁCH 2: bắn event nếu menu-active.js có lắng nghe
-      document.dispatchEvent(new Event("menu:loaded"));
-    });
-
   function toggleMenu() {
     const nav = document.getElementById("nav-main");
     if (!nav) return;
